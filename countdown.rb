7ea9603a5811e0ece001a7acca7aee49c27d8114
Charlie <peabody00@gmail.com>
2020-12-14 09:39:45 -0600

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(countdown_output)
+    while countdown_output > 0
+        puts "#{countdown_output} SECOND(S)!" 
+        countdown_output -= 1
+    end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countdown_output)
+    while countdown_output > 0
+        puts "#{countdown_output} SECOND(S)!" 
+        countdown_output -= 1
+        sleep(1)
+    end
+return "HAPPY NEW YEAR!"
 end
